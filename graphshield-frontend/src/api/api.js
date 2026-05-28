cbca7d346ef6f3fefe7b6847c534546c9deb6cc3
Shrinivas <shrinivasrj734@gmail.com>
2026-05-29 01:17:36 +0530

    env file

--- graphshield-frontend/src/api/api.js
+++ graphshield-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || 'http://localhost:8080/api'
+  process.env.REACT_APP_API_URL || 'http://localhost:8080/api'
 
 const api = axios.create({
   baseURL: BASE_URL,
